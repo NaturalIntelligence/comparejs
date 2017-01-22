976e76f74616b82b8a94a8e451cae848b7e739c2
Amit Gupta <amitgupta.gwl@gmail.com>
2017-01-22 12:48:39 +0000

    Add profile entry and reports

--- benchmark/profile.js
+++ benchmark/profile.js
@@ -12,5 +12,32 @@ var profile = {
 		"description" : "An optimised Jsonpath parser",
 		"link" : ["https://www.npmjs.com/package/fastpath", "https://github.com/pvenkatakrishnan/fastPath"] ,
 		"logopath" : ""
+	},
+	"fast-lorem-ipsum" : { 
+		"id" : "fast-lorem-ipsum" ,
+		"name" : "fast-lorem-ipsum",
+		"description" : "Generate Lorem Ipsum string fast",
+		"link" : ["https://www.npmjs.com/package/fast-lorem-ipsum", "https://github.com/amitguptagwl/fast-lorem-ipsum"] ,
+		"logopath" : ""
+	},
+	"npm-jsonpath-plus" : { 
+		"id" : "npm-jsonpath-plus" ,
+		"name" : "jsonpath-plus",
+		"description" : "Analyse, transform, and selectively extract data from JSON documents (and JavaScript objects).",
+		"link" : ["https://www.npmjs.com/package/jsonpath-plus/", "https://github.com/s3u/JSONPath"] ,
+		"logopath" : ""
+	},
+	"lorem-ipsum" : { 
+		"id" : "lorem-ipsum" ,
+		"name" : "lorem-ipsum",
+		"description" : "Generates passages of lorem ipsum text suitable for use as placeholder copy in web pages, graphics, and more.",
+		"link" : ["https://www.npmjs.com/package/lorem-ipsum", "https://github.com/knicklabs/node-lorem-ipsum"] ,
+		"logopath" : ""
+	},
+	"vanila-jsonpath" : { 
+		"id" : "vanila-jsonpath" ,
+		"name" : "vanila-jsonpath",
+		"description" : "Found somewhere on the net",
+		"logopath" : ""
 	}
 }
